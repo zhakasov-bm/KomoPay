b1fe9c6968b5ed8ea14d8c4c1a736de150973741
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2025-03-28 02:36:14 +0500

    Update images

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Home />
       <FeatureList />
       <About />
-      <Services id="services" />
+      <Services />
       <Partners />
       <Advantages />
       <Contact />
