52affff56df66baeb70d670edd751cdcc88d3c00
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2025-03-28 03:04:40 +0500

    Update image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ function About() {
             <div className="about-cards">
                 <img src="/about/1.svg" alt="about-image-1" />
                 <img src="/about/2.svg" alt="" />
-                <img src="/about/3.pdf" alt="" />
+                <img src="/about/3.svg" alt="" />
             </div>
         </section>
     )
