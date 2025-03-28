5c98f5003fbb781f15080631511f8c2a5fe0f2f0
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2025-03-28 11:39:05 +0500

    Commit

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -22,16 +22,18 @@ const partnersLogos = [
 ];
 
 function Partners() {
-    return(
+    return (
         <section id="partners">
             <h2>Наши партнеры</h2>
-            <div className="partners-logo">
-                {partnersLogos.map((logo, index) => (
-                    <img key={index} src={logo} alt={`Partner ${index + 1}`} />
-                ))}
+            <div className="partners-slider">
+                <div className="partners-logo">
+                    {partnersLogos.map((logo, index) => (
+                        <img key={index} src={logo} alt={`Partner ${index + 1}`} />
+                    ))}
+                </div>
             </div>
         </section>
-    )
+    );
 }
 
 export default Partners;
