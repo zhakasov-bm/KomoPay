387f82cd3c9a0592e7a137245c08f0adc10a3aae
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2025-04-04 18:39:32 +0500

    Fix partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,28 +1,43 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const partnersLogos = [
-    "/partners/yandex.svg",
-    "/partners/rbk.svg",
-    "/partners/jusan.svg",
-    "/partners/simply.svg",
-    "/partners/turing.svg",
-    "/partners/bope.svg",
-    "/partners/parking.svg"
+  "/partners/yandex.svg",
+  "/partners/rbk.svg",
+  "/partners/jusan.svg",
+  "/partners/simply.svg",
+  "/partners/turing.svg",
+  "/partners/bope.svg",
+  "/partners/parking.svg",
 ];
 
 function Partners() {
-    return (
-        <section id="partners">
-            <h2>Наши партнеры</h2>
-            <div className="partners-slider">
-                <div className="partners-logo">
-                    {partnersLogos.map((logo, index) => (
-                        <img key={index} src={logo} alt={`Partner ${index + 1}`} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="partners">
+      <h2>Наши партнеры</h2>
+
+      {/* Mobile auto-scroll */}
+      <div className="mobile-only">
+        <Marquee speed={70} gradient={false} pauseOnHover={false}> {/* Disable pause on hover */}
+          {partnersLogos.map((logo, index) => (
+            <img
+              key={index}
+              src={logo}
+              alt={`Partner ${index + 1}`}
+              className="partner-logo-mobile"
+            />
+          ))}
+        </Marquee>
+      </div>
+
+      {/* Desktop grid */}
+      <div className="partners-logo desktop-only">
+        {partnersLogos.map((logo, index) => (
+          <img key={index} src={logo} alt={`Partner ${index + 1}`} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Partners;
